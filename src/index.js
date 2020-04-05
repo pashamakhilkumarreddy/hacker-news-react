163e6aa1d8ec7dee862024a513468645dec266d7
pashamakhilkumarreddy <akhil.reddy@frequence.com>
2020-04-05 23:52:25 +0530

    Add Project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../node_modules/bulma/css/bulma.css'
+import '../node_modules/bulma/css/bulma.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
